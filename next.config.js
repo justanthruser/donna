49fdaa860a745c9571391d3e6a20cab1a4d193e1
dominicOT <dominicoladapotonade@gmail.com>
2025-06-21 18:14:26 -0400

    next config js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    // Disable the warning about preloaded CSS not being used
+    optimizeCss: false,
+  },
+  // Disable the warning about preloaded CSS not being used
+  onRecoverableError: (err) => {
+    if (err.message.includes('was preloaded using link preload but not used')) {
+      return;
+    }
+    console.error(err);
+  },
 };
  
 module.exports = nextConfig;
